317815fe1c7b1b14c0db32aa9309d37743cb3d9a
Niilo <niilo.keinanen@arction.com>
2019-10-20 14:37:28 +0300

    Init project

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app.ts',
+    module:{
+        rules:[
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization:{
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'LCJS streaming data showcase',
+            template: './src/index.html'
+        }),
+        new CopyWebpackPlugin([
+            { from: 'src/GitHub-Mark-White-32px.png' }
+        ])
+    ]
+}
